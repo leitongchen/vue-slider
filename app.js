98578427814d647704139321649efcd84ffced72
leitong <leitongchen93@gmail.com>
2021-04-30 16:10:19 +0200

    on dot click cambia immagine

--- app.js
+++ app.js
@@ -2,9 +2,44 @@ const app = new Vue({
 
     el: "#appContainer",
     data: {
-        ciao: 'ciao',
+        imgSource: "img/illustration__0004_14_fox.jpg",
+        illustrations: [
+            {
+                indice: 1,
+                imgSource: "img/illustration__0004_14_fox.jpg"
+            },
+            {
+                indice: 2,
+                imgSource: "img/illustration__0005_11_Bones-snake.jpg"
+            },
+            {
+                indice: 3,
+                imgSource: "img/illustration__0006_10_Artist.jpg"
+            },
+            {
+                indice: 4,
+                imgSource: "img/illustration__lantern.jpg"
+            },
+            {
+                indice: 5,
+                imgSource: "img/illustration__0000_sophia-lowres.jpg"
+            },
+            {
+                indice: 6,
+                imgSource: "img/illustration__0001_July_.jpg"
+            },
+            {
+                indice: 7,
+                imgSource: "img/illustration__0002_Black-Lady_fullcolor-min.jpg"
+            },
+        ],
+
+
+
     },
     methods: {
-
+        onDotClick(event) {
+            this.imgSource = event.imgSource;
+        }
     },
 });
